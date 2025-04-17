c8fff2a3425ac27852ff02fae12ce1302928a555
s <w@MacBookPro.ht.home>
2025-04-16 23:53:05 -0500

    initial commit with first class

--- src/intro_to_Java/Lesson01_HelloWorld.java
+++ src/intro_to_Java/Lesson01_HelloWorld.java
@@ -1,4 +1,25 @@
 package intro_to_Java;
 
 public class Lesson01_HelloWorld {
+
+    public static void main(String[] args) {
+
+        // We can use // to write a single line comment
+
+        /*
+        Used to write comments
+         that span multiple lines
+
+        Java knows that comment lines
+          are not related to its execution
+          and it does not execute them.
+
+         Their color is different from the code,
+          they are gray.
+         */
+
+        System.out.println("Hello World");
+
+
+    }
 }
