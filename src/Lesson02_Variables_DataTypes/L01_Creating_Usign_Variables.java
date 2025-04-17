24d6c2fccce6ad0f754d5725ee640b35eb315985
s <w@MacBookPro.ht.home>
2025-04-17 00:12:22 -0500

    Add current value of number variable code

---
+++ src/Lesson02_Variables_DataTypes/L01_Creating_Usign_Variables.java
@@ -0,0 +1,32 @@
+package Lesson02_Variables_DataTypes;
+
+public class L01_Creating_Usign_Variables {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        // DATA TYPE ==> int
+        // VARIABLE NAME ===> age
+        // assigment sing ====> =
+        // value ====> 20
+        // the cursor that indicates the end of code ====> ;
+
+        // print the value of the age variable to the console
+        System.out.println(" the value of the varialbe ");
+
+        // print the value of the age variable
+        System.out.println(age);
+
+        int number = 10;
+
+        // print the current value of the number variable to the screen
+
+        System.out.println(number);
+
+
+
+
+
+        //
+    }
+}
