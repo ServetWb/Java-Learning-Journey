bab2cc4cd172f716d123c9de2c8912b4c09c3cfc
servet <swbb34@hotmail.com>
2025-05-25 22:30:17 -0500

    Sum Of Numbers In Between example2

--- src/Lesson15_ForLoop/L04_SumOfNumbersInBetween.java
+++ src/Lesson15_ForLoop/L04_SumOfNumbersInBetween.java
@@ -2,14 +2,13 @@ package Lesson15_ForLoop;
 
 import java.util.Scanner;
 
-public class L03_SumOfNumbersInBetween{
-
+public class L04_SumOfNumbersInBetween {
     public static void main(String[] args) {
+
         // Question 3:
         // Ask the user to enter starting and ending values as positive integers.
         // Print the sum of all numbers between them, including the boundaries.
-        // If the ending value is smaller than the starting value,
-        // display a warning and terminate the operation.
+        // The code should work even if the ending value is smaller than the starting value.
 
         Scanner scanner = new Scanner(System.in);
 
@@ -21,16 +20,19 @@ public class L03_SumOfNumbersInBetween{
 
         int sum = 0;
 
-        if (end < start) {
-            System.out.println("The ending value cannot be smaller than the starting value...");
-        } else {
+        if (start < end) {
             for (int i = start; i <= end; i++) {
+                sum += i;
+            }
+        } else {
+            // start > end
+            for (int i = start; i >= end; i--) {
                 sum += i;
             }
         }
 
         System.out.println("The sum of the numbers in between: " + sum);
 
-        scanner.close(); // Good practice to close the scanner
+        scanner.close(); // Always good practice to close the scanner
     }
 }
